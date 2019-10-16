8253e74dda158003e1392dc8e2c5388f929fa5f4
Jason Stewart <jason.iowa@gmail.com>
2019-10-16 13:56:43 -0600

    Initial Commit

---
+++ Project 2 Stewart Jason/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project 2 Stewart Jason
+//
+//  Created by Student on 10/16/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
